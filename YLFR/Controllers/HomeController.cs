739bbb345bc396634a51a2b265f12738addba98e
cmacivor <Caesar2810$>
2014-12-05 13:53:38 -0500

    initial page is working

--- YLFR/Controllers/HomeController.cs
+++ YLFR/Controllers/HomeController.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YLFR.Models;
+using YLFR.ViewModels;
 
 namespace YLFR.Controllers
 {
     public class HomeController : Controller
     {
+        private UnitOfWork unitOfWork = null;
+
+        public HomeController()
+            : this(new UnitOfWork())
+        {
+
+        }
+
+        public HomeController(UnitOfWork uow)
+        {
+            this.unitOfWork = uow;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -16,7 +31,7 @@ namespace YLFR.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            
             return View();
         }
 
@@ -26,5 +41,47 @@ namespace YLFR.Controllers
 
             return View();
         }
+
+        public ActionResult Page1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Page1(Page1ViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                Applicant applicant = new Applicant
+                {
+                    FirstName = vm.FirstName,
+                    MiddleName = vm.MiddleName,
+                    LastName = vm.LastName,
+                    Address = vm.Address,
+                    Phone = vm.Phone,
+                    BirthDate = vm.BirthDate,
+                    Gender = vm.Gender,
+                    Employer = vm.Employer,
+                    BusinessPhone = vm.BusinessPhone,
+                    Email = vm.Email,
+                    HighestLevelEducation = vm.EducationLevel,
+                    AdvancedEducation = vm.AdvancedEducation,
+                    EmergencyContactName = vm.EmergencyContactName,
+                    EmergencyContactRelationship = vm.EmergencyContactRelationship,
+                    EmergencyContactPhone = vm.EmergencyContactPhone,
+                    HowDidYouHear = vm.HowDidYouHear,
+                    WhyAreYouInterested = vm.WhyAreYouInterested,
+                    PreviousVolunteerExperience = vm.PreviousVolunteerExperience,
+                    HobbiesTalentSkills = vm.HobbiesTalentSkills,
+                    CommunityChurchAffiliations = vm.CommunityChurchAffiliations
+                };
+
+                unitOfWork.ApplicantRepository.AddApplicant(applicant);
+                unitOfWork.ApplicantRepository.Save();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
---
+++ YLFR/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YLFR
+{
+    public class UnitOfWork
+    {
+        private MentorshipAppllicantsEntities entities = null;
+
+        //This will be called from controller default constructor
+        public UnitOfWork()
+        {
+            entities = new MentorshipAppllicantsEntities();
+            ApplicantRepository = new ApplicantRepository(entities);
+        }
+
+
+        // This will be created from test project and passed on to the
+        // controllers parameterized constructors
+        public UnitOfWork(IApplicantRepository applicantRepo)
+        {
+            ApplicantRepository = applicantRepo;
+        }
+
+
+        public IApplicantRepository ApplicantRepository
+        {
+            get;
+            private set;
+        }
+    }
+}
